43c73178cded68cd6b73e949dc8ec026c0fdd8b8
Kirk Charles Niverba <kirkniverba23@gmail.com>
2020-01-26 18:16:27 +0800

    Re: Main Screen

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class HomePage extends StatelessWidget {
       extendBodyBehindAppBar: true,
       body: SlidingUpPanel(
         backdropEnabled: true,
-        minHeight: MediaQuery.of(context).size.height * 0.12,
+        minHeight: MediaQuery.of(context).size.height * 0.10,
         borderRadius: BorderRadius.vertical(top: Radius.circular(50)),
         panel: Theme(
             data: ThemeData(brightness: Brightness.light), child: Panel()),
@@ -37,14 +37,14 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         leading: Icon(Icons.settings),
         title: Text(
-          'UnoVPN',
+          'UniVPN',
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
         actions: <Widget>[
           Container(
               margin: EdgeInsets.only(right: 16),
-              child: Icon(Icons.network_wifi)),
+              child: Icon(Icons.person_outline)),
         ],
         elevation: 0,
         backgroundColor: Colors.transparent,
@@ -58,7 +58,7 @@ class Panel extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Text(
-        'Sliding Widget',
+        'Pahingi Load',
         style: TextStyle(color: Colors.black),
       ),
     );
@@ -67,7 +67,7 @@ class Panel extends StatelessWidget {
 
 class Collapsed extends StatelessWidget {
   final TextStyle textStyle =
-      TextStyle(color: Colors.black, fontWeight: FontWeight.w500);
+      TextStyle(color: Colors.black, fontWeight: FontWeight.w600);
 
   @override
   Widget build(BuildContext context) {
@@ -111,6 +111,11 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+          color: Color(0xff1F275D),
+          image: DecorationImage(
+              image: AssetImage("images/background/background.jpg"),
+              fit: BoxFit.cover)),
       child: SafeArea(
         child: Container(
           child: Column(
@@ -120,11 +125,65 @@ class Body extends StatelessWidget {
                 child: Column(
                   children: <Widget>[
                     Expanded(
-                      child: Placeholder(),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text('Status: '),
+                          Text(
+                            'Not Connected',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              color: Colors.pink,
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                     Expanded(
-                      flex: 3,
-                      child: Placeholder(),
+                      flex: 4,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: IntrinsicHeight(
+                          child: AspectRatio(
+                            aspectRatio: 1,
+                            child: GestureDetector(
+                              onTap: () => print('tapped'),
+                              child: Stack(
+                                alignment: Alignment.center,
+                                children: <Widget>[
+                                  ClipOval(
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                          gradient: LinearGradient(
+                                              colors: <Color>[
+                                            Color(0xffAE61FF),
+                                            Color(0xff7861FF)
+                                          ],
+                                              begin: Alignment.topCenter,
+                                              end: Alignment.bottomCenter)),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(25),
+                                    child: ClipOval(
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                            gradient: LinearGradient(
+                                                begin: Alignment.topCenter,
+                                                end: Alignment.bottomCenter,
+                                                colors: <Color>[
+                                              Color(0xff192152),
+                                              Color(0xff141C49)
+                                            ])),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
                   ],
                 ),
@@ -136,11 +195,149 @@ class Body extends StatelessWidget {
                     child: Column(
                       children: <Widget>[
                         Expanded(
-                          flex: 1,
-                          child: Placeholder(),
+                          child: IntrinsicHeight(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: <Widget>[
+                                Expanded(
+                                  child: Align(
+                                      alignment: Alignment.bottomCenter,
+                                      child: Text('Select Location')),
+                                ),
+                                Expanded(
+                                  flex: 2,
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 16, horizontal: 32),
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                          color: Color(0xff202A60),
+                                          borderRadius: BorderRadius.all(
+                                              Radius.circular(50))),
+                                      child: Row(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: <Widget>[
+                                          Expanded(
+                                            child: Padding(
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      vertical: 14),
+                                              child: Center(
+                                                child: AspectRatio(
+                                                  aspectRatio: 1,
+                                                  child: ClipOval(
+                                                    child: Container(
+                                                      color: Colors.white,
+                                                    ),
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                          Expanded(
+                                            flex: 3,
+                                            child: Text(
+                                              'Germany',
+                                              textAlign: TextAlign.start,
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.w600,
+                                                  fontSize: 24),
+                                            ),
+                                          ),
+                                          Expanded(
+                                            child: Center(
+                                                child: Icon(
+                                              Icons.keyboard_arrow_down,
+                                              color: Color(0xff8961E1),
+                                              size: 40,
+                                            )),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
                         ),
                         Expanded(
-                          child: Placeholder(),
+                          child: IntrinsicHeight(
+                            child: FractionallySizedBox(
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.fromLTRB(32, 8, 32, 16),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      color: Color(0xff825FFE),
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(24))),
+                                  child: Row(
+                                    children: <Widget>[
+                                      Expanded(
+                                        flex: 2,
+                                        child: AspectRatio(
+                                          aspectRatio: 1,
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(24.0),
+                                            child: Center(
+                                              child: Container(
+                                                decoration: BoxDecoration(
+                                                    color: Colors.white,
+                                                    borderRadius:
+                                                        BorderRadius.all(
+                                                            Radius.circular(
+                                                                30))),
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        flex: 5,
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: <Widget>[
+                                            Expanded(
+                                              child: Padding(
+                                                padding:
+                                                    const EdgeInsets.symmetric(
+                                                        vertical: 20),
+                                                child: Column(
+                                                  crossAxisAlignment:
+                                                      CrossAxisAlignment.start,
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment
+                                                          .spaceEvenly,
+                                                  children: <Widget>[
+                                                    Text(
+                                                        'Start your free 3 days trial',
+                                                        style: TextStyle(
+                                                            fontSize: 22,
+                                                            fontWeight:
+                                                                FontWeight
+                                                                    .bold)),
+                                                    Text(
+                                                      'Go Pro to unlock all features',
+                                                      style: TextStyle(
+                                                          fontWeight:
+                                                              FontWeight.w300),
+                                                    )
+                                                  ],
+                                                ),
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
                         ),
                       ],
                     )),
